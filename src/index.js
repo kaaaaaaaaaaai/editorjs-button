2965623683871dde1c41e60620049cbc372e401b
kaaaaaaaai <kaiogita@gmail.com>
2024-04-23 15:05:06 +0900

    トグルの削除とtuneに統合

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import css from './index.scss';
-
+import svg from './toolbox-icon.svg'
 export default class AnyButton {
     /**
      *
@@ -8,8 +8,35 @@ export default class AnyButton {
     static get toolbox(){
         return {
             title:"Button",
-            icon: '<svg xmlns="http://www.w3.org/2000/svg" id="Layer_1" enable-background="new 0 0 512 512" height="20" viewBox="0 0 512 512" width="20"><path d="m237.102 366v-90.018h-90c-11.046 0-20-8.954-20-20s8.954-20 20-20h90v-90.982c0-11.046 8.954-20 20-20s20 8.954 20 20v90.982h90c11.046 0 20 8.954 20 20s-8.954 20-20 20h-90v90.018c0 11.046-8.954 20-20 20s-20-8.954-20-20zm254.898-15c11.046 0 20-8.954 20-20v-251c0-44.112-35.888-80-80-80h-352c-44.112 0-80 35.888-80 80v352c0 44.112 35.888 80 80 80h352c44.112 0 80-35.888 80-80 0-11.046-8.954-20-20-20s-20 8.954-20 20c0 22.056-17.944 40-40 40h-352c-22.056 0-40-17.944-40-40v-352c0-22.056 17.944-40 40-40h352c22.056 0 40 17.944 40 40v251c0 11.046 8.954 20 20 20z"/></svg>'
+            icon:svg
         }
+    }
+
+    static get tunes() {
+        return [
+            {
+                name: 'edit_mode',
+                icon: svg,
+                title: 'ボタンを編集',
+            }
+        ]
+    }
+    renderSettings() {
+        const tunes = AnyButton.tunes;
+
+        return tunes.map(tune => ({
+            icon: tune.icon,
+            label: this.api.i18n.t(tune.title),
+            name: tune.name,
+            onActivate: () => {
+                console.log("onActivate")
+                this.data = {
+                    "link": this.nodes.linkInput.textContent,
+                    "text": this.nodes.textInput.textContent
+                }
+                this.show(Number(AnyButton.STATE.EDIT))
+            }
+        }));
     }
 
     /**
@@ -126,9 +153,6 @@ export default class AnyButton {
             registButton: "anyButtonContainer__registerButton",
             anyButtonHolder: "anyButtonContainer__anyButtonHolder",
             btnColor: "anyButton__btn--default",
-            toggleSwitch: "toggle-switch",
-            toggleInput: "toggle-input",
-            toggleLabel: "toggle-label",
         }
 
         this.CSS = Object.assign(_CSS, config.css)
@@ -147,13 +171,10 @@ export default class AnyButton {
 
         //入力用
         this.nodes.inputHolder = this.makeInputHolder();
-        //toggle
-        this.nodes.toggleHolder = this.makeToggle();
         //display button
         this.nodes.anyButtonHolder = this.makeAnyButtonHolder();
 
 
-        this.nodes.container.appendChild(this.nodes.toggleHolder);
         this.nodes.container.appendChild(this.nodes.inputHolder);
         this.nodes.container.appendChild(this.nodes.anyButtonHolder);
 
@@ -227,43 +248,13 @@ export default class AnyButton {
             case AnyButton.STATE.EDIT:
                 this.nodes.inputHolder.classList.remove(this.CSS.hide);
                 this.nodes.anyButtonHolder.classList.add(this.CSS.hide);
-                this.nodes.toggleInput.checked = 0;
 
                 break;
             case AnyButton.STATE.VIEW:
                 this.nodes.inputHolder.classList.add(this.CSS.hide);
                 this.nodes.anyButtonHolder.classList.remove(this.CSS.hide);
-                this.nodes.toggleInput.checked = 1;
                 break;
         }
-    }
-
-    makeToggle(){
-        /**
-         * <div class="toggle-switch">
-         <input id="toggle" class="toggle-input" type='checkbox' />
-         <label for="toggle" class="toggle-label"/>
-         </div>
-         */
-        const toggleHolder = this.make('div', [this.CSS.toggleSwitch]);
-        this.nodes.toggleInput = this.make('input', [this.CSS.toggleInput],
-            {
-                "type":"checkbox",
-                "id":"toggle"
-            });
-        const label = this.make('label', [this.CSS.toggleLabel],{"for":"toggle"});
-
-        this.nodes.toggleInput.addEventListener("change", (event) => {
-            this.data = {
-                "link": this.nodes.linkInput.textContent,
-                "text": this.nodes.textInput.textContent
-            }
-            this.show(Number(this.nodes.toggleInput.checked))
-        })
-        toggleHolder.appendChild(this.nodes.toggleInput);
-        toggleHolder.appendChild(label);
-
-        return toggleHolder;
     }
 
     /**
